3416abcfc60de380ed0037af74ececabdd715ef2
amithash <amithash@aa7a1653-0e4d-0410-86da-b1735d549bc1>
2008-10-09 22:39:04 +0000

    Moved dschedule to the modules dir.

--- ThesisModules/dschedule/estimate.h
+++
@@ -1,5 +0,0 @@
-
-
-void put_mask_from_stats(struct task_struct *ts);
-void get_state_of_cpu(int *state);
-void put_state_of_cpu(void);
--- ThesisModules/dschedule/quanta.h
+++
@@ -1,4 +0,0 @@
-
-void state_change(unsigned long param);
-int create_timer(void);
-void destroy_timer(void);
