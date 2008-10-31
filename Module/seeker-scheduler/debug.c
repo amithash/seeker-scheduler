021862a6d16bb1435445841d5922d9d30becfecc
amithash <amithash@aa7a1653-0e4d-0410-86da-b1735d549bc1>
2008-10-31 23:21:36 +0000

    FIxed possible issues with debug unit. Hopefully this might get rid of the module removal problems.

--- Module/seeker-scheduler/debug.c
+++ Module/seeker-scheduler/debug.c
@@ -44,28 +44,41 @@ static int dev_open = 0;
 static struct kmem_cache *debug_cachep = NULL;
 
 
-
+/* Returns a pointer and takes a lock if allocation is
+ * successful. Do not waste time. Fill it and call
+ * put_debug asap! */
 struct debug_block *get_debug(void)
 {
 	struct debug_block *p = NULL;
 	if(!dev_open)
 		return NULL;
+	if(unlikely(!current_debug))
+		return NULL;
+	if(unlikely(!debug_cachep)){
+		debug("debug_cachep is not initialized..");
+		return NULL;
+	}
 	spin_lock(&debug_lock);
+	/* Just in case this was waiting for the lock
+	 * and meanwhile, purge just set current_debug
+	 * to NULL. */
 	if(unlikely(!current_debug)){
-		goto out;
+		spin_unlock(&debug_lock);
+		return NULL;
 	}
 	p = (struct debug_block *)kmem_cache_alloc(debug_cachep, GFP_ATOMIC);
-	if(!p)
-		goto out;
+	if(!p){
+		debug("Allocation failed");
+		spin_unlock(&debug_lock);
+		return NULL;
+	}
 	current_debug->next = p;
 	p->next = NULL;
 	current_debug = p;
 	return p;
-out:
-	spin_unlock(&debug_lock);
-	return NULL;
 }
 
+/* Releases the spinlock */
 void put_debug(struct debug_block *p)
 {
 	if(p)
@@ -86,20 +99,19 @@ void purge_debug(void)
 	unsigned int flags;
 	if(start_debug == NULL || current_debug == NULL)
 		return;
-
+	/* Acquire the lock, then set current debug to NULL
+	 */
 	spin_lock_irqsave(&debug_lock,flags);
 	c1 = start_debug;
 	start_debug = NULL;
 	current_debug = NULL;
+	spin_unlock_irqrestore(&debug_lock,flags);
 	while(c1){
 		c2 = c1->next;
 		debug_free(c1);
 		c1 = c2;
 	}
-	spin_unlock_irqrestore(&debug_lock,flags);
 }
-	
-
 
 ssize_t seeker_debug_read(struct file *file_ptr, char __user *buf, 
 			      size_t count, loff_t *offset)
@@ -144,28 +156,37 @@ int seeker_debug_close(struct inode *in, struct file *f)
 
 int debug_init(void)
 {
+	if(unlikely(misc_register(&seeker_debug_mdev) < 0)){
+		error("seeker_debug device register failed");
+		return -1;
+	}
+
 	debug_cachep = kmem_cache_create("seeker_debug_cache",
 					 sizeof(struct debug_block),
 					 0,
 					 SLAB_PANIC,
 					 NULL);
-	/* If creating the cache failed, then do not enable
-	 * the read interface. 
-	 */
 	if(!debug_cachep){
 		error("Could not create debug cache, Debug unit will not be avaliable");
-		return -1;
+		goto err;
 	}
 	current_debug = kmem_cache_alloc(debug_cachep, GFP_ATOMIC);
-	start_debug = current_debug;
-	spin_lock_init(&debug_lock);
-	first_read = 1;
-	if(unlikely(misc_register(&seeker_debug_mdev) < 0)){
-		error("seeker_debug device register failed");
-		return -1;
+	if(!current_debug){
+		error("Initial allocation from the cache failed");
+		kmem_cache_destroy(debug_cachep);
+		goto err;
 	}
+	start_debug = current_debug;
+	start_debug->next = NULL;
+	spin_lock_init(&debug_lock);
+
 	dev_created = 1;
+	first_read = 1;
 	return 0;
+err:
+	error("Something went wrong in the debug init section. It will be unavaliable based on the implementation of the caller");
+	misc_deregister(&seeker_debug_mdev);
+	return -1;
 }
 
 void debug_exit(void)
@@ -173,11 +194,11 @@ void debug_exit(void)
 	if(dev_open)
 		dev_open = 0;
 
-	if(dev_created)
+	if(dev_created){
 		misc_deregister(&seeker_debug_mdev);
-
-	purge_debug();
-	kmem_cache_destroy(debug_cachep);
-	dev_created = 0;
+		purge_debug();
+		kmem_cache_destroy(debug_cachep);
+		dev_created = 0;
+	}
 }
 
