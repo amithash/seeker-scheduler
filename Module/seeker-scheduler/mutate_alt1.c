5106026ec9f5146c3a04216815903dd3bff759ea
amithash <amithash@aa7a1653-0e4d-0410-86da-b1735d549bc1>
2008-11-04 20:42:47 +0000

    Added a mutate_alt1 just to add a new way to do things without hampering what works....

---
+++ Module/seeker-scheduler/mutate_alt1.c
@@ -0,0 +1,242 @@
+#include <linux/kernel.h>
+#include <linux/init.h>
+#include <linux/module.h>
+#include <linux/sched.h>
+#include <linux/cpumask.h>
+
+#include <seeker.h>
+#include <scpufreq.h>
+
+#include "state.h"
+#include "debug.h"
+
+
+static int new_cpu_state[NR_CPUS];
+static int state_matrix[NR_CPUS][MAX_STATES];
+extern struct state_desc states[MAX_STATES];
+extern unsigned int max_state_in_system;
+extern int total_online_cpus;
+extern int max_allowed_states[NR_CPUS];
+extern int cur_cpu_state[NR_CPUS];
+
+u64 interval_count;
+
+inline int procs(int hints,int total, int total_load);
+
+inline int procs(int hints,int total, int total_load)
+{
+	int ans;
+	if(hints == 0)
+		return 0;
+	if(hints == total)
+		return total_load;
+	
+	ans = div((hints * total_load),total) - 1;
+	return ans < 0 ? 0 : ans;
+}
+
+void update_state_matrix(int delta)
+{
+	int i,j,k;
+	for(i=0;i<total_online_cpus;i++){
+		for(j=new_cpu_state[i],k=0; j<max_state_in_system; j++,k++){
+			if(k>delta)
+				state_matrix[i][j] = 0;
+			else
+				state_matrix[i][j] = (max_state_in_system-k);
+		}
+
+		for(j=new_cpu_state[i]-1,k=1; j>=0; j--,k++){
+			if(k>delta)
+				state_matrix[i][j] = 0;
+			else
+				state_matrix[i][j] = (max_state_in_system-k);
+		}
+	}
+}
+
+void choose_layout(int delta)
+{
+	int total = 0;
+	int demand[MAX_STATES];
+	int d;
+	int load = 0;
+	struct debug_block *p = NULL;
+	unsigned int i,j,k,l;
+	int winner=0;
+	unsigned int winner_val = 0;
+	unsigned int winner_best_proc = 0;
+	unsigned int winner_best_proc_value = 0;
+	unsigned int winner_best_low_proc_value = 0;
+	unsigned int best_proc = 0;
+	unsigned int best_proc_value = 0;
+	unsigned int best_low_proc_value = 0;
+	unsigned long irq_flags;
+	int poison[NR_CPUS];
+	int sum;
+	int total_iter = 0;
+
+	interval_count++;
+	if(delta < 1)
+		return;
+
+	/* Create a state matrix such that, the cell which
+	 * indicates a processors current state, gets the highest
+	 * value = iax_state_in_system^2, and parabolically decreases on either side.
+	 */
+	for(i=0;i<total_online_cpus;i++){
+		poison[i] = 1;
+		new_cpu_state[i] = cur_cpu_state[i];
+		load += weighted_cpuload(i) >= SCHED_LOAD_SCALE ? 1 : 0;
+	}
+
+	/* Total Hint */
+	
+	for(j=0;j<max_state_in_system;j++){
+		total += states[j].demand;
+		demand[j] = 0;
+	}
+
+	/* Num of cpus required for this state 
+	 * SUM(demand[]) could be < cpus. 
+	 * Make sure to bring down their states. */
+	for(j=0;j<max_state_in_system;j++){
+		d = procs(states[j].demand,total,load);
+		if(d == 0)
+			continue;
+		for(k=j,l=0;(d-l) > 0 && k<max_state_in_system;k++,l++)
+			demand[k] += (d-l);
+		for(k=j-1,l=1;(d-l)>0 && k>=0;k--,l++)
+			demand[k] += (d-l);
+		debug("required cpus for state %d = %d",j,demand[j]);
+	}
+
+	/* Now for each delta to spend, hold an auction */
+	do{
+		winner = 0;
+		winner_val = 0;
+		winner_best_proc = 0;
+		winner_best_proc_value = 0;
+		winner_best_low_proc_value = 0;
+		total_iter++;
+	
+		debug("Iteration %d",total_iter);
+
+		update_state_matrix(delta);
+
+		/* There is an optimization here, so do not get confused.
+		 * Technically, each column in the state matrix is supposed
+		 * to be multiplied by the demand. But that is done here,
+		 * as the demand decreases once won.
+		 */
+
+		/* For each state, */
+		for(j=0;j<max_state_in_system;j++){
+			sum = 0;
+			best_proc = 0;
+			best_proc_value = 0;
+			best_low_proc_value = -1;
+
+			/* Sum the cost over all rows */
+			for(i=0;i<total_online_cpus;i++){
+				if((state_matrix[i][j] * poison[i]) > best_proc_value){
+					best_proc_value = state_matrix[i][j] * poison[i] * demand[j];
+					best_proc = i;
+				} else if(state_matrix[i][j] < best_low_proc_value){
+					best_low_proc_value = state_matrix[i][j] * demand[j];
+				}
+				sum += (state_matrix[i][j] * poison[i]);
+			}
+
+			sum = sum * (demand[j]+1);
+			debug("sum for state %d is %d with demand %d",j,sum,demand[j]);
+
+			/* Find the max sum and the sate, and its best proc 
+			 * If there is contention for that, choose the one
+			 * with the best proc, if there is contention for both,
+			 * choose the one with the best lowest proc,
+			 * if there is contention for that too, then first come
+			 * first serve */
+			if(sum < winner_val)
+				continue;
+			if(sum > winner_val) 
+				goto assign;
+			if(best_proc_value < winner_best_proc_value)
+				continue;
+			if(best_proc_value > winner_best_proc_value)
+				goto assign;
+			if(best_low_proc_value < winner_best_low_proc_value)
+				continue;
+
+assign:
+			winner = j;
+			winner_val = sum;
+			winner_best_proc= best_proc;
+			winner_best_proc_value = best_proc_value;
+			winner_best_low_proc_value = best_low_proc_value;
+
+		}
+		/* A winning val of 0 indicated a failed auction.
+		 * all contenstents are broke. Go home loosers.*/
+		if(winner_val <= 0)
+			break;
+
+		debug("Winner is state %d choosing cpu %d",winner,winner_best_proc);
+
+		/* Now the winning state, reduces its demand */
+		if(demand[winner] > 0)
+			demand[winner]--;
+
+		/* The best processor is best_proc */
+		/* Poison the choosen processor element */
+		poison[winner_best_proc] = 0;
+
+		/* Subtract that from the delta */
+		delta -= ABS(cur_cpu_state[winner_best_proc] - winner);
+
+		/* Assign the new cpus state to be the winner */
+		new_cpu_state[winner_best_proc] = winner;
+
+		/* Continue the auction if delta > 0  or till all cpus are allocated */
+	} while(delta > 0 && total_iter < total_online_cpus);
+
+	p = get_debug(&irq_flags);
+	if(p){
+		p->entry.type = DEBUG_MUT;
+		p->entry.u.mut.interval = interval_count;
+		p->entry.u.mut.count = max_state_in_system;
+	}
+
+	mark_states_inconsistent();
+	for(j=0;j<max_state_in_system;j++){
+		states[j].cpus = 0;
+		cpus_clear(states[j].cpumask);
+		if(p){
+			p->entry.u.mut.cpus_req[j] = demand[j];
+			p->entry.u.mut.cpus_given[j] = 0;
+		}
+		states[j].demand = 0;
+	}
+
+
+	for(i=0;i<total_online_cpus;i++){
+		/* XXX This violates DELTA. But this 
+		 * also makes sure that unused processors 
+		 * are in the lowest cpu state */
+		if(poison[i] == 1)
+			new_cpu_state[i] = 0;
+		
+		if(new_cpu_state[i] != cur_cpu_state[i]){
+			cur_cpu_state[i] = new_cpu_state[i];
+			set_freq(i,new_cpu_state[i]);
+			if(p)
+				p->entry.u.mut.cpus_given[cur_cpu_state[i]]++;
+		}
+		states[cur_cpu_state[i]].cpus++;
+		cpu_set(i,states[cur_cpu_state[i]].cpumask);
+	}
+	mark_states_consistent();
+	put_debug(p,&irq_flags);
+}
+
+
