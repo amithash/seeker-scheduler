5583b26494cd3dc25589785bd54f74e174160cb5
amithash <amithash@aa7a1653-0e4d-0410-86da-b1735d549bc1>
2008-11-12 07:48:01 +0000

    daemonized powersample.

--- Scripts/powersample.c
+++ Scripts/powersample.c
@@ -8,17 +8,22 @@ int main(int argc,char *argv[])
 {
 	char touch[200];
 	char cat[200];
+	pid_t pid;
 	if(argc != 2){
 		printf("Usage: %s FILE\n",argv[0]);
 		exit(1);
 	}
-	sprintf(touch,"/usr/bin/touch %s",argv[1]);
+	sprintf(touch,"/usr/bin/echo \" \" > %s",argv[1]);
 	sprintf(cat,"/usr/bin/ipmitool sensor get /SYS/VPS | /bin/grep -i \"sensor reading\" >> %s",argv[1]);
-
 	system(touch);
-	while(1){
-		system(cat);
-		usleep(1000000);
+	pid = fork();
+	if(pid != 0){
+		exit(EXIT_SUCCESS);
+	} else {
+		while(1){
+			system(cat);
+			usleep(1000000);
+		}
 	}
 	return 0;
 }
