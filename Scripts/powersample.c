91c48365ff9fdf958b48b370f2660e3f067d2d21
amithash <amithash@aa7a1653-0e4d-0410-86da-b1735d549bc1>
2008-11-12 07:58:08 +0000

    Added interval to powersample

--- Scripts/powersample.c
+++ Scripts/powersample.c
@@ -8,6 +8,7 @@ int main(int argc,char *argv[])
 {
 	char touch[200];
 	char cat[200];
+	char interval[200];
 	pid_t pid;
 	if(argc != 2){
 		printf("Usage: %s FILE\n",argv[0]);
@@ -15,14 +16,16 @@ int main(int argc,char *argv[])
 	}
 	sprintf(touch,"/usr/bin/echo \" \" > %s",argv[1]);
 	sprintf(cat,"/usr/bin/ipmitool sensor get /SYS/VPS | /bin/grep -i \"sensor reading\" >> %s",argv[1]);
+	sprintf(interval,"/bin/date +\"\%s\" >> %s",argv[1]);
+
 	system(touch);
 	pid = fork();
 	if(pid != 0){
 		exit(EXIT_SUCCESS);
 	} else {
 		while(1){
+			system(interval);
 			system(cat);
-			usleep(1000000);
 		}
 	}
 	return 0;
