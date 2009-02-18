6983d4bcb56c8f8d558f2c9eb246fa3ed0fc8082
root <root@boulder-desktop.(none)>
2009-02-17 18:09:24 -0700

    added bench_states script

---
+++ AnalysisScripts/bench_stats.pl
@@ -0,0 +1,77 @@
+#!/usr/bin/perl
+
+use strict;
+use warnings;
+
+my %stats = (	'[0.00,0.25)' => 0,
+   		'[0.25,0.50)' => 0,
+   		'[0.50,0.75)' => 0,
+   		'[0.75,1.00)' => 0,
+   		'[1.00,1.25)' => 0,
+   		'[1.25,1.50)' => 0,
+   		'[1.50,1.75)' => 0,
+   		'[1.75,2.00)' => 0,
+   		'[2.00,2.25)' => 0,
+   		'[2.25,5.00)' => 0,
+   		'[5.00,inf)' => 0
+	);
+
+if($#ARGV != 0){
+	print "USAGE: $0 file";
+	exit;
+}
+
+
+
+sub calassify{
+	my $ipc = shift;
+	my $cy = shift;
+
+	if($ipc >= 0 and $ipc < 0.25){
+		$stats{'[0.00,0.25)'} += $cy;
+		return;
+	}
+	if($ipc >= 0.25 and $ipc < 0.5){
+		$stats{'[0.25,0.50)'} += $cy;
+		return;
+	}
+	if($ipc >= 0.5 and $ipc < 0.75){
+		$stats{'[0.50,0.75)'} += $cy;
+		return;
+	}
+	if($ipc >= 0.75 and $ipc < 1.0){
+		$stats{'[0.75,1.00)'} += $cy;
+		return;
+	}
+	if($ipc >= 1.0 and $ipc < 1.25){
+		$stats{'[1.00,1.25)'} += $cy;
+		return;
+	}
+	if($ipc >= 1.25 and $ipc < 1.5){
+		$stats{'[1.25,1.50)'} += $cy;
+		return;
+	}
+	if($ipc >= 1.50 and $ipc < 1.75){
+		$stats{'[1.50,1.75)'} += $cy;
+		return;
+	}
+	if($ipc >= 1.75 and $ipc < 2.0){
+		$stats{'[1.75,2.00)'} += $cy;
+		return;
+	}
+	if($ipc >= 2.0 and $ipc < 2.25){
+		$stats{'[2.00,2.25)'} += $cy;
+		return;
+	}
+	if($ipc >= 2.25 and $ipc < 5.0){
+		$stats{'[2.25,5.00)'} += $cy;
+		return;
+	}
+	if($ipc >= 5.0){
+		$stats{'[5.00,inf)'} += $cy;
+		return;
+	}
+	#should never be executed.
+	print "error";
+}
+
