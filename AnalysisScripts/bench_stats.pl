2886a0622247d69dea3e54d8de9085c96af40ff0
Amithash Prasad <prasadae@linux-w6q8.(none)>
2009-02-17 19:27:52 -0700

    completed bench stats.

--- AnalysisScripts/bench_stats.pl
+++ AnalysisScripts/bench_stats.pl
@@ -15,13 +15,51 @@ my %stats = (	'[0.00,0.25)' => 0,
    		'[2.25,5.00)' => 0,
    		'[5.00,inf)' => 0
 	);
+my %residency;
 
 if($#ARGV != 0){
 	print "USAGE: $0 file";
 	exit;
 }
 
+my $total = 0.0;
 
+open IN, "$ARGV[0]" or die "Opening $ARGV[0] failed with $!\n";
+while(my $line = <IN>){
+	chomp($line);
+	my @l = split(/,/,$line);
+	my $ipc = $l[0];
+	my $cy = $l[1];
+	my $state = $l[2];
+	classify($ipc,$cy * 1.0);
+	residency($state,$cy * 1.0);
+	$total += ($cy*1.0);
+}
+
+foreach my $ipc (keys %stats){
+	if($stats{$ipc} == 0.0){
+		next;
+	}
+	my $perc = $stats{$ipc} * 100 / $total;
+	my $perc_s = sprintf("%.2f",$perc);
+	print "$ipc\t$perc_s\n";
+}
+print "\n\n";
+foreach my $state (sort keys %residency){
+	my $cyb = $residency{$state} / (10.0 ** 9);
+	my $cyb_s = sprintf("%.4f",$cyb);
+	print "$state\t$cyb_s\n";
+}
+
+sub residency{
+	my $state = shift;
+	my $cy = shift;
+	if(defined($residency{$state})){
+		$residency{$state} += $cy;
+	} else {
+		$residency{$state} = $cy;
+	}
+}
 
 sub calassify{
 	my $ipc = shift;
