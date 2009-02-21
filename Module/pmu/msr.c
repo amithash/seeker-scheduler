9b4ca4854ebf7848bc31d49e6f2cfa2830c82b36
Amithash Prasad <prasadae@linux-w6q8.(none)>
2009-02-21 13:01:00 -0700

    1. Fixed a bug where the evtsel[cpu][counter] was disabled on counter_disable but not counters[cpu][counter]. But counter_enable checks the latter thus causing re-use to fail.
    added comments on what bits are modified.

--- Module/pmu/msr.c
+++ Module/pmu/msr.c
@@ -34,8 +34,6 @@
 
 #include "pmu_int.h"
 
-#define BITS8 ((1<<8)-1)
-
 extern evtsel_t evtsel[NR_CPUS][NUM_COUNTERS];
 extern counter_t counters[NR_CPUS][NUM_COUNTERS];
 extern cleared_t cleared[NR_CPUS][NUM_COUNTERS];
@@ -62,6 +60,7 @@ inline void evtsel_clear(u32 evtsel_num)
 	u32 low, high;
 	rdmsr(evtsel[0][evtsel_num].addr, low, high);
 	low &= EVTSEL_RESERVED_BITS;
+	high &= EVTSEL_RESERVED_BITS_HIGH;
 	wrmsr(evtsel[0][evtsel_num].addr, low, high);
 #endif
 }
@@ -80,23 +79,22 @@ inline void evtsel_write(u32 evtsel_num)
 		rdmsr(cur_evtsel->addr, low, high);
 		low &= EVTSEL_RESERVED_BITS;
 
-		low |= ((cur_evtsel->ev_select & BITS8) << 0)
-		    | (cur_evtsel->ev_mask << 8)
-		    | (cur_evtsel->usr_flag << 16)
-		    | (cur_evtsel->os_flag << 17)
-		    | (cur_evtsel->edge << 18)
-		    | (cur_evtsel->pc_flag << 19)
-		    | (cur_evtsel->int_flag << 20)
-		    | (cur_evtsel->enabled << 22)
-		    | (cur_evtsel->inv_flag << 23)
-		    | (cur_evtsel->cnt_mask << 24);
+		low |= ((cur_evtsel->ev_select & BITS(8)) << 0) /*bits 0-7 */
+		    | (cur_evtsel->ev_mask << 8) /* bits 8-15 */
+		    | (cur_evtsel->usr_flag << 16) /* bit 16 */
+		    | (cur_evtsel->os_flag << 17) /* bit 17 */
+		    | (cur_evtsel->edge << 18) /* bit 18 */
+		    | (cur_evtsel->pc_flag << 19) /* bit 19 */
+		    | (cur_evtsel->int_flag << 20) /* bit 20 */
+		    | (cur_evtsel->enabled << 22) /* bit 22 */
+		    | (cur_evtsel->inv_flag << 23) /* bit 23 */
+		    | (cur_evtsel->cnt_mask << 24); /* bits 24-31 */
 
-#if defined(ARCH_K8) || defined(ARCH_K10)
 		high &= EVTSEL_RESERVED_BITS_HIGH;
-
-		high |= ((cur_evtsel->ev_select >> 8) << 0)
-			| (cur_evtsel->go << 8)
-			| (cur_evtsel->ho << 9);
+#if defined(ARCH_K8) || defined(ARCH_K10)
+		high |= ((cur_evtsel->ev_select >> 8) << 0) /* bits 0(32)-3(35) */
+			| (cur_evtsel->go << 8) /* bit 8(40) */
+			| (cur_evtsel->ho << 9); /* bit 9(41) */
 #endif
 
 		wrmsr(cur_evtsel->addr, low, high);
@@ -152,8 +150,7 @@ u64 get_counter_data(u32 counter, u32 cpu_id)
 	if (unlikely(counter >= NUM_COUNTERS))
 		return 0;
 	counter_val = (u64) counters[cpu_id][counter].low;
-	counter_val =
-	    counter_val | ((u64) counters[cpu_id][counter].high << 32);
+	counter_val |= ((u64) counters[cpu_id][counter].high << 32);
 	return counter_val;
 #else
 	return 0;
@@ -167,15 +164,14 @@ inline void counter_disable(int counter)
 {
 #if NUM_COUNTERS > 0
 	int cpu_id = smp_processor_id();
-	if (unlikely(counter >= NUM_COUNTERS))
+	if (unlikely(counter >= NUM_COUNTERS || cpu_id >= NR_CPUS))
 		return;
 
-	if (likely(cpu_id < NR_CPUS)) {
-		evtsel_clear(counter);
-		counter_clear(counter);
-		evtsel[cpu_id][counter].enabled = 0;
-		evtsel_write(counter);
-	}
+	evtsel_clear(counter);
+	counter_clear(counter);
+	evtsel[cpu_id][counter].enabled = 0;
+	counters[cpu_id][counter].enabled = 0;
+	evtsel_write(counter);
 #endif
 }
 
@@ -197,28 +193,27 @@ int counter_enable(u32 event, u32 ev_mask, u32 os)
 			break;
 		}
 	}
-	if (likely(counter_num >= 0)) {
-		evtsel_clear(counter_num);
-		counter_clear(counter_num);
-		counters[cpu_id][counter_num].enabled = 1;
-		counters[cpu_id][counter_num].event = event;
-		counters[cpu_id][counter_num].mask = ev_mask;
-		//counfigure the event sel reg
-		evtsel[cpu_id][counter_num].ev_select = event;
-		evtsel[cpu_id][counter_num].ev_mask = ev_mask;
-		evtsel[cpu_id][counter_num].usr_flag = 1;
-		evtsel[cpu_id][counter_num].os_flag = (os & 1);
-		evtsel[cpu_id][counter_num].pc_flag = 1;
-		evtsel[cpu_id][counter_num].int_flag = 0;
-		evtsel[cpu_id][counter_num].inv_flag = 0;
-		evtsel[cpu_id][counter_num].cnt_mask = 0;
-		evtsel[cpu_id][counter_num].enabled = 1;
-		evtsel[cpu_id][counter_num].edge = 0;
-		evtsel_write(counter_num);
-		return counter_num;
-	} else {
+	if(unlikely(counter_num < 0))
 		return -1;
-	}
+
+	evtsel_clear(counter_num);
+	counter_clear(counter_num);
+	counters[cpu_id][counter_num].enabled = 1;
+	counters[cpu_id][counter_num].event = event;
+	counters[cpu_id][counter_num].mask = ev_mask;
+	//counfigure the event sel reg
+	evtsel[cpu_id][counter_num].ev_select = event;
+	evtsel[cpu_id][counter_num].ev_mask = ev_mask;
+	evtsel[cpu_id][counter_num].usr_flag = 1;
+	evtsel[cpu_id][counter_num].os_flag = (os & 1);
+	evtsel[cpu_id][counter_num].pc_flag = 1;
+	evtsel[cpu_id][counter_num].int_flag = 0;
+	evtsel[cpu_id][counter_num].inv_flag = 0;
+	evtsel[cpu_id][counter_num].cnt_mask = 0;
+	evtsel[cpu_id][counter_num].enabled = 1;
+	evtsel[cpu_id][counter_num].edge = 0;
+	evtsel_write(counter_num);
+	return counter_num;
 #else
 	return -1;
 #endif
