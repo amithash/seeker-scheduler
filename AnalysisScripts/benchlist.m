0c023ba3f66941819ce772bc110b81a12b240577
Amithash Prasad <prasadae@linux-w6q8.(none)>
2009-02-22 16:19:28 -0700

    Updated benchlist.m to not have the prefix benchmark numbers.

--- AnalysisScripts/benchlist.m
+++ AnalysisScripts/benchlist.m
@@ -2,31 +2,31 @@
 # name: bench
 # type: sq_string
 # elements: 14
-# length: 14
-401.bzip2     
-# length: 14
-483.xalancbmk 
-# length: 14
-458.sjeng     
-# length: 14
-462.libquantum
-# length: 14
-456.hmmer     
-# length: 14
-445.gobmk     
-# length: 14
-464.h264ref   
-# length: 14
-471.omnetpp   
-# length: 14
-429.mcf       
-# length: 14
-473.astar     
-# length: 14
-482.sphinx3   
-# length: 14
-470.lbm       
-# length: 14
-433.milc      
-# length: 14
-453.povray    
+# length: 10
+bzip2     
+# length: 10
+xalancbmk 
+# length: 10
+sjeng     
+# length: 10
+libquantum
+# length: 10
+hmmer     
+# length: 10
+gobmk     
+# length: 10
+h264ref   
+# length: 10
+omnetpp   
+# length: 10
+mcf       
+# length: 10
+astar     
+# length: 10
+sphinx3   
+# length: 10
+lbm       
+# length: 10
+milc      
+# length: 10
+povray    
