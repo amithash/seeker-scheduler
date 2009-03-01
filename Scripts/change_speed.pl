9d805caadd1f68ffa74cfba5c12ec61f020dff72
Amithash Prasad <prasadae@linux-w6q8.(none)>
2009-02-28 17:33:57 -0700

    finished change_speed. Now takes a pattern.

--- Scripts/change_speed.pl
+++ Scripts/change_speed.pl
@@ -3,13 +3,29 @@
 use strict;
 use warnings;
 use Getopt::Long;
+use Time::HiRes;
 
 my $s_pattern;
-GetOptions('p|pattern=s'   => \$s_pattern);
+my $interval;
+GetOptions('p|pattern=s'   => \$s_pattern,
+	   'i|interval=i'  => \$interval);
 
+# Defaults.
+$s_pattern = "0000" unless(defined($s_pattern));
+$interval = 1000 unless(defined($interval));
+
+# Get real pattern. 
 my @pattern = split(//,$s_pattern);
 
-# validate.
+# Get total online cpus.
+my $total_cpus = `cat /proc/cpuinfo | grep MHz | wc -l`;
+chomp($total_cpus);
+$total_cpus = int($total_cpus);
+
+# Get max states a core can have.
+my $max = get_max_states();
+
+# validate pattern.
 for(my $i=0;$i < scalar @pattern; $i++){
 	if($pattern[$i] =~ /^\d$/){
 		$pattern[$i] = int($pattern[$i]);
@@ -17,8 +33,20 @@ for(my $i=0;$i < scalar @pattern; $i++){
 		print "Pattern $s_pattern contains non-numeric values\n";
 		exit;
 	}
+	if($pattern[$i] >= $max){
+		print "$pattern[$i] (${i}th from the left) is not a valid state\n";
+		print "Valid states are in the interval [0,$max)\n";
+		exit;
+	}
 }
-my $max = get_max_states();
+
+#Kernel. 
+while(1){
+	foreach my $p (@pattern){
+		set_speed_all($p);
+		usleep($interval * 1000);
+	}
+}
 
 sub get_max_states{
 	my $l = `cat /sys/devices/system/cpu/cpu0/cpufreq/scaling_available_frequencies`;
@@ -26,6 +54,14 @@ sub get_max_states{
 	my @a = split(/\s+/,$l);
 	return scalar @a;
 }
+
+sub set_speed_all{
+	my $state = shift;
+	for(my $i=0;$i<$total_cpus;$i++){
+		set_speed($i,$state);
+	}
+}
+
 
 sub set_speed{
 	my $cpu = shift;
