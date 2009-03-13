d4738ac64648522348d3759953aa2b8c5e070732
Amithash Prasad <prasadae@linux-491i.site>
2009-03-12 20:38:51 -0700

    changed root to my home dir.

--- AnalysisScripts/fixed.sh
+++ AnalysisScripts/fixed.sh
@@ -1,15 +1,15 @@
 #!/bin/bash
 
-rm -r /root/group_4
-rm -r /root/group_8
-mkdir /root/group_4
-mkdir /root/group_8
+rm -r /home/prasadae/group_4
+rm -r /home/prasadae/group_8
+mkdir /home/prasadae/group_4
+mkdir /home/prasadae/group_8
 
 function mailme {
-	cat /root/fixed.err > /root/tmp_mail
-	cat /root/fixed.out >> /root/tmp_mail
-	echo "Regards, AMDBox" >> /root/tmp_mail
-	cat /root/tmp_mail | mail -s "$1" amithash@gmail.com
+	cat /home/prasadae/fixed.err > /home/prasadae/tmp_mail
+	cat /home/prasadae/fixed.out >> /home/prasadae/tmp_mail
+	echo "Regards, AMDBox" >> /home/prasadae/tmp_mail
+	cat /home/prasadae/tmp_mail | mail -s "$1" amithash@gmail.com
 }
 
 A=`lsmod | grep powernow_k8 | wc -l`
@@ -57,23 +57,23 @@ for (( i=0;i<$CPUS_LEN;i++ )); do
 	for (( j=0;j<$LAYOUT_LEN;j++ )); do
 		echo "layout=${LAYOUT[$j]}"
 		echo "layout Name=${LAYOUT_NAME[$j]}"
-		insmod $SEEKER_HOME/Build/seeker_scheduler.ko static_layout=${LAYOUT[$j]} allowed_cpus=${CPUS[$i]} >> /root/fixed.out 2>> /root/fixed.err
+		insmod $SEEKER_HOME/Build/seeker_scheduler.ko static_layout=${LAYOUT[$j]} allowed_cpus=${CPUS[$i]} >> /home/prasadae/fixed.out 2>> /home/prasadae/fixed.err
 		for (( k=0;k<$BLS_LEN;k++ )); do
 			${SEEKER_HOME}/AnalysisScripts/generate_runscript.pl --benchlist=${SEEKER_HOME}/AnalysisScripts/group_${CPUS[$i]}/${BLS[$k]} -a 
 			chmod +x ./run.sh
-			$SEEKER_HOME/Scripts/debugd /root/group_${CPUS[$i]}/log_ >> /root/fixed.out 2>> /root/fixed.err
+			$SEEKER_HOME/Scripts/debugd /home/prasadae/group_${CPUS[$i]}/log_ >> /home/prasadae/fixed.out 2>> /home/prasadae/fixed.err
 			sleep 10
 			rm ./LOG
-			$BENCH_ROOT/run.sh >> /root/fixed.out 2>> /root/fixed.err
+			$BENCH_ROOT/run.sh >> /home/prasadae/fixed.out 2>> /home/prasadae/fixed.err
 			sleep 10
-			$SEEKER_HOME/Scripts/send.pl -t >> /root/fixed.out 2>> /root/fixed.err
-			$SEEKER_HOME/AnalysisScripts/ex_time.pl ./LOG /root/group_${CPUS[$i]}/time_${LAYOUT_NAME[$j]}__${BLS_NAME[$k]}.txt >> /root/fixed.out 2>> /root/fixed.err
-			$SEEKER_HOME/Scripts/decodelog < /root/group_${CPUS[$i]}/log_0 > /root/group_${CPUS[$i]}/raw_${LAYOUT_NAME[$j]}_${BLS_NAME[$k]}.txt 
-			$SEEKER_HOME/Scripts/pull.pl --input /root/group_${CPUS[$i]}/raw_${LAYOUT_NAME[$j]}_${BLS_NAME[$k]}.txt --output /root/group_${CPUS[$i]}/log_${LAYOUT_NAME[$j]}_${BLS_NAME[$k]} --benchlist ${SEEKER_HOME}/AnalysisScripts/group_${CPUS[$i]}/${BLS[$k]} --what=all >> /root/fixed.out 2>> /root/fixed.err
-			rm /root/group_${CPUS[$i]}/log_0 >> /root/fixed.out 2>> /root/fixed.err
+			$SEEKER_HOME/Scripts/send.pl -t >> /home/prasadae/fixed.out 2>> /home/prasadae/fixed.err
+			$SEEKER_HOME/AnalysisScripts/ex_time.pl ./LOG /home/prasadae/group_${CPUS[$i]}/time_${LAYOUT_NAME[$j]}__${BLS_NAME[$k]}.txt >> /home/prasadae/fixed.out 2>> /home/prasadae/fixed.err
+			$SEEKER_HOME/Scripts/decodelog < /home/prasadae/group_${CPUS[$i]}/log_0 > /home/prasadae/group_${CPUS[$i]}/raw_${LAYOUT_NAME[$j]}_${BLS_NAME[$k]}.txt 
+			$SEEKER_HOME/Scripts/pull.pl --input /home/prasadae/group_${CPUS[$i]}/raw_${LAYOUT_NAME[$j]}_${BLS_NAME[$k]}.txt --output /home/prasadae/group_${CPUS[$i]}/log_${LAYOUT_NAME[$j]}_${BLS_NAME[$k]} --benchlist ${SEEKER_HOME}/AnalysisScripts/group_${CPUS[$i]}/${BLS[$k]} --what=all >> /home/prasadae/fixed.out 2>> /home/prasadae/fixed.err
+			rm /home/prasadae/group_${CPUS[$i]}/log_0 >> /home/prasadae/fixed.out 2>> /home/prasadae/fixed.err
 			mailme "Cpus=${CPUS[$i]}, Layout=${LAYOUT_NAME[$j]}, bench group=${BLS_NAME[$k]}, just got over"
-			rm /root/fixed.out
-			rm /root/fixed.err
+			rm /home/prasadae/fixed.out
+			rm /home/prasadae/fixed.err
 		done
 		rmmod seeker_scheduler
 	done
