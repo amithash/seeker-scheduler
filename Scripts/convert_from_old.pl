fed9527e30eca3341098db3933358f165327057c
Amithash Prasad <aeprasad@aeprasad-laptop.(none)>
2009-03-25 17:23:01 -0600

    Fixed a few issues with the migrationFromOld script.

--- Scripts/convert_from_old.pl
+++ Scripts/convert_from_old.pl
@@ -10,9 +10,9 @@ my %grouph = (
 	"High" => "HighIPC0",
 	"Low" => "LowIPC1",
 	"Low-High" => "Low-HighIPC0",
-	"PLow-Low" => "PhaseLow-LowIPC",
-	"Plow-High" => "PhaseLow-High",
-	"PHigh-PHigh" => "PhaseHigh-PhaseLow"
+	"PLow-Low" => "PhaseLow-Low",
+	"PLow-High" => "PhaseLow-High",
+	"PHigh-PLow" => "PhaseHigh-PhaseLow"
 );
 
 my @files = split(/\n/, join("", `ls $cur/raw_*.txt`));
@@ -21,6 +21,7 @@ foreach my $f (@files){
 	if($f =~ /raw_(\d_\d)_(.+)\.txt$/){
 		my $level = $1;
 		my $group = $2;
+		print "Doing $level, $group = $grouph{$group}\n";
 		system("rm -r log_$level\_$group");
 		system("$ENV{SEEKER_HOME}/Scripts/pull.pl --input $f --output $cur/log_$level\_$group --benchlist $ENV{SEEKER_HOME}/AnalysisScripts/group_4/$grouph{$group}");
 	}
