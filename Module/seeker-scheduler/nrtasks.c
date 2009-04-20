58c1d8858e8b87646df708658774fafcf6947e49
Amithash Prasad <aeprasad@aeprasad-laptop.(none)>
2009-04-20 17:27:50 -0600

    added api to use get_cpu_nr_running to take the load from there.

---
+++ Module/seeker-scheduler/nrtasks.c
@@ -0,0 +1,60 @@
+/*****************************************************
+ * Copyright 2008 Amithash Prasad                    *
+ *                                                   *
+ * This file is part of Seeker                       *
+ *                                                   *
+ * Seeker is free software: you can redistribute     *
+ * it and/or modify it under the terms of the        *
+ * GNU General Public License as published by        *
+ * the Free Software Foundation, either version      *
+ * 3 of the License, or (at your option) any         *
+ * later version.                                    *
+ *                                                   *
+ * This program is distributed in the hope that      *
+ * it will be useful, but WITHOUT ANY WARRANTY;      *
+ * without even the implied warranty of              *
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR       *
+ * PURPOSE. See the GNU General Public License       *
+ * for more details.                                 *
+ *                                                   *
+ * You should have received a copy of the GNU        *
+ * General Public License along with this program.   *
+ * If not, see <http://www.gnu.org/licenses/>.       *
+ *****************************************************/
+
+#include <linux/kernel_stat.h>
+#include <linux/percpu.h>
+#include <linux/sched.h>
+#include <linux/cpumask.h>
+
+extern int total_online_cpus;
+
+extern struct state_desc states[MAX_STATES];
+
+/********************************************************************************
+ * 				Functions					*
+ ********************************************************************************/
+
+int get_tasks_load(void)
+{
+	int total = 0;
+	int i;
+	for(i = 0; i < total_online_cpus; i++) {
+		total += get_cpu_nr_running(i);
+	}
+	return total > total_online_cpus ? total_online_cpus : total;
+}
+
+int get_state_tasks(int state)
+{
+	int i; 
+	int tasks = 0;
+	cpumask_t mask = states[state].cpumask;
+	for_each_cpu_mask(i,mask){
+		tasks += get_cpu_nr_running(i);
+	}
+	return tasks > states[state].cpus ? states[state].cpus : tasks;
+}
+
+
+}
