b3a2e067450da7989f4d6786bf3c457ecae01566
Amithash Prasad <aeprasad@terminator.(none)>
2009-09-07 14:23:09 -0600

    added a few more things.

--- generate_callgraph.pl
+++ generate_callgraph.pl
@@ -24,39 +24,39 @@ print OUT "\trankdir=LR\n";
 
 # Print Function structure.
 foreach my $file (keys %files_all){
-  if((not defined($file_funcs{$file})) and (not defined($file_macros{$file}))){
-    next;
-  }
-  print OUT "/* $file */\n";
-  print OUT "\tsubgraph cluster_$files_all{$file} {\n";
-  print OUT "\t\tlabel=\"$file\"\n";
-  print OUT "\t\tshape=\"rectangle\"\n";
-  if(defined($file_macros{$file})){
-    my $ref = $file_macros{$file};
-    foreach my $func (@$ref){
-      print OUT "\t\tmacro_$files_all{$file}_$func [label=\"$func\",color=\"lightgrey\",style=\"filled\"]\n";
+    if((not defined($file_funcs{$file})) and (not defined($file_macros{$file}))){
+        next;
     }
-  }
-  if(defined($file_funcs{$file})){
-    my $ref = $file_funcs{$file};
-    foreach my $func (@$ref){
-      print OUT "\t\tfunc_$files_all{$file}_$func [label=\"$func\"]\n";
+    print OUT "/* $file */\n";
+    print OUT "\tsubgraph cluster_$files_all{$file} {\n";
+    print OUT "\t\tlabel=\"$file\"\n";
+    print OUT "\t\tshape=\"rectangle\"\n";
+    if(defined($file_macros{$file})){
+        my $ref = $file_macros{$file};
+        foreach my $func (@$ref){
+            print OUT "\t\tmacro_$files_all{$file}_$func [label=\"$func\",color=\"lightgrey\",style=\"filled\"]\n";
+        }
     }
-  }
-  print OUT "\t}\n\n";
+    if(defined($file_funcs{$file})){
+        my $ref = $file_funcs{$file};
+        foreach my $func (@$ref){
+            print OUT "\t\tfunc_$files_all{$file}_$func [label=\"$func\"]\n";
+        }
+    }
+    print OUT "\t}\n\n";
 }
 
 # Parse calls.
 my $proc_call_string = parse_calls(@ARGV);
 my @ext_funcs = keys %external_funcs;
 if(scalar @ext_funcs > 0){
-  print OUT "\tsubgraph cluster_EXT_funcs {\n";
-  print OUT "\t\tlabel=\"ExternalDependancies\"\n";
-  print OUT "\t\tshape=\"rectangle\"\n";
-  foreach my $ext (@ext_funcs){
-    print OUT "\t\tEXT_$ext [label=\"$ext\"]\n";
-  }
-  print OUT "\t}\n";
+    print OUT "\tsubgraph cluster_EXT_funcs {\n";
+    print OUT "\t\tlabel=\"ExternalDependancies\"\n";
+    print OUT "\t\tshape=\"rectangle\"\n";
+    foreach my $ext (@ext_funcs){
+        print OUT "\t\tEXT_$ext [label=\"$ext\"]\n";
+    }
+    print OUT "\t}\n";
 }
 print OUT "\n\n";
 print OUT "$proc_call_string\n";
@@ -66,352 +66,362 @@ print OUT "$proc_call_string\n";
 # Print Footer.
 print OUT "}\n";
 
-
-# XXX Print the function structures based on files. 
-
-# Everything is in %funcs and %macros. 
-# XXX From here, as and when we find a use, we start adding an arrow. 
-# Design descision, uses of functions not defined goes in a seperate DUMMY FILE called "ExternalDependancies"
-# Make the ExternalDependancies structure "editable" so people can re-group them...
 close(OUT);
+
+foreach my $a (${$func_files{"TS_MEMBER"}}){
+    print "$a\n";
+}
 
 sub parse_def
 {
-  my @args = @_;
-  foreach my $f (@args){
-  	if(-e $f){
-  		parse_def_file($f);
-  	}
-  	if(-d $f){
-  		parse_def_dir($f);
-  	}
-  }
+    my @args = @_;
+    foreach my $f (@args){
+        if(-e $f){
+            parse_def_file($f);
+        }
+        if(-d $f){
+            parse_def_dir($f);
+        }
+    }
 }
 
 sub parse_def_dir
 {
-	my $d = shift;
-	my @cont = <$d/*>;
-	foreach my $f (@cont){
-		if(-e $f){
-			parse_def_file($f);
-		}
-		if(-d $f){
-			parse_def_dir($f);
-		}
-	}
+    my $d = shift;
+    my @cont = <$d/*>;
+    foreach my $f (@cont){
+        if(-e $f){
+            parse_def_file($f);
+        }
+        if(-d $f){
+            parse_def_dir($f);
+        }
+    }
 }
 
 sub parse_calls
 {
-  my @args = @_;
-  my $call_string = "";
-  foreach my $f (@args){
-  	if(-e $f){
-  		$call_string = parse_calls_file($f,$call_string);
-  	}
-  	if(-d $f){
-  		$call_string = parse_calls_dir($f, $call_string);
-  	}
-  }
-  return $call_string;
+    my @args = @_;
+    my $call_string = "";
+    foreach my $f (@args){
+        if(-e $f){
+            $call_string = parse_calls_file($f,$call_string);
+        }
+        if(-d $f){
+            $call_string = parse_calls_dir($f, $call_string);
+        }
+    }
+    return $call_string;
 }
 
 sub parse_calls_dir
 {
-	my $d = shift;
-  my $call_string = shift;
-	my @cont = <$d/*>;
-	foreach my $f (@cont){
-		if(-e $f){
-			$call_string = parse_calls_file($f,$call_string);
-		}
-		if(-d $f){
-			$call_string = parse_calls_dir($f,$call_string);
-		}
-	}
-  return $call_string;
+    my $d = shift;
+    my $call_string = shift;
+    my @cont = <$d/*>;
+    foreach my $f (@cont){
+        if(-e $f){
+            $call_string = parse_calls_file($f,$call_string);
+        }
+        if(-d $f){
+            $call_string = parse_calls_dir($f,$call_string);
+        }
+    }
+    return $call_string;
 }
 
 sub parse_def_file
 {
-	my $inf = shift;
+    my $inf = shift;
 
-  if(-d $inf){
-    return;
-  }
-  if($inf !~ /\.c$/ and $inf !~ /\.h$/){
-    return;
-  }
-  $files_all{$inf} = $file_count;
-  $file_count++;
-	open IN, "$inf" or die "Could not open $inf\n";
-	while(my $line = <IN>){
-		chomp($line);
-		my $func_name = is_func_def($line);
-		if($inf =~ /\.c$/){
-			if($func_name ne "NULL"){
-        if(defined($file_funcs{$inf})){
-          push @{$file_funcs{$inf}}, $func_name;
-        } else {
-          $file_funcs{$inf} = [$func_name];
+    if(-d $inf){
+        return;
+    }
+    if($inf !~ /\.c$/ and $inf !~ /\.h$/){
+        return;
+    }
+    $files_all{$inf} = $file_count;
+    $file_count++;
+    open IN, "$inf" or die "Could not open $inf\n";
+    while(my $line = <IN>){
+        chomp($line);
+        my $func_name = is_func_def($line);
+        if($inf =~ /\.c$/){
+            if($func_name ne "NULL"){
+                if(defined($file_funcs{$inf})){
+                    push @{$file_funcs{$inf}}, $func_name;
+                } else {
+                    $file_funcs{$inf} = [$func_name];
+                }
+                if(defined($func_files{$func_name})){
+                    if(exists_in_array($inf,$func_files{$func_name})){
+                        print STDERR "WARNING: Re-def of function $func_name in the same file $inf assumed to be featurization\n";
+                    } else {
+                        push @{$func_files{$func_name}}, $inf;
+                    }
+                } else {
+                    $func_files{$func_name} = [$inf];
+                }
+                next;
+            }
         }
-        if(defined($func_files{$func_name})){
-          if(exists_in_array($inf,$func_files{$func_name})){
-            print STDERR "WARNING: Re-def of function $func_name in the same file $inf assumed to be featurization\n";
-          } else {
-            push @{$func_files{$func_name}}, $inf;
-          }
-        } else {
-          $func_files{$func_name} = [$inf];
-        }
-				next;
-			}
-		}
-		$func_name = is_macro($line);
-		if($func_name ne "NULL"){
-        if(defined($file_macros{$inf})){
-          push @{$file_macros{$inf}}, $func_name;
-        } else {
-          $file_macros{$inf} = [$func_name];
-        }
-        if(defined($macro_files{$func_name})){
-          if(exists_in_array($inf,$macro_files{$func_name})){
-            print STDERR "WARNING: Re-def of macro $func_name in the same file $inf assumed to be featurization\n";
-          } else {
-            push @{$macro_files{$func_name}}, $inf;
-          }
-        } else {
-          $macro_files{$func_name} = [$inf];
-        }
+        $func_name = is_macro($line);
+        if($func_name ne "NULL"){
+                if(defined($file_macros{$inf})){
+                    push @{$file_macros{$inf}}, $func_name;
+                } else {
+                    $file_macros{$inf} = [$func_name];
+                }
+                if(defined($macro_files{$func_name})){
+                    if(exists_in_array($inf,$macro_files{$func_name})){
+                        print STDERR "WARNING: Re-def of macro $func_name in the same file $inf assumed to be featurization\n";
+                    } else {
+                        push @{$macro_files{$func_name}}, $inf;
+                    }
+                } else {
+                    $macro_files{$func_name} = [$inf];
+                }
 
-			next;
-		}
-	}
-	close(IN);
+            next;
+        }
+    }
+    close(IN);
 }
 
 sub parse_calls_file
 {
-  my $f = shift;
-  my $func_regexp = "[A-Za-z_0-9][A-Za-z_0-9]+";
-  my $call_string = shift;
-  open IN, "$f" or die "Could not open $f\n";
-  my $current_func = "";
+    my $f = shift;
+    my $func_regexp = "[A-Za-z_0-9][A-Za-z_0-9]+";
+    my $call_string = shift;
+    open IN, "$f" or die "Could not open $f\n";
+    my $current_func = "";
 
-  my $processing_func = 0;
-  my $processing_macro = 0;
-  my $indent_count = 0;
-  my $at_least_one = 0;
-  while(my $line = <IN>){
-    chomp($line);
-    my $func_name = is_func_def($line);
-    if($f =~ /\.c$/){
-      if($func_name ne "NULL"){
-        $current_func = $func_name;
-        $processing_func = 1;
-        if($line =~ /{/){
-          $indent_count++;
-          $at_least_one = 1;
+    my $processing_func = 0;
+    my $processing_macro = 0;
+    my $indent_count = 0;
+    my $waiting_func_start = 0;
+    while(my $line = <IN>){
+        chomp($line);
+        my $opening_brace = count_char("{", $line);
+        my $closing_brace = count_char("}", $line);
+        my $indent_overflow = $opening_brace - $closing_brace;
+        if(($indent_count + $indent_overflow) >= 0){
+            $indent_count += $indent_overflow;
         }
-        next;
-      }
-    }
-    $func_name = is_macro($line);
-    my $is_macro = 0;
-    if($func_name ne "NULL"){
-      $is_macro = 1;
-      $current_func = $func_name;
-      $processing_macro = 1;
-      $processing_func = 0;
-    }
-    if($current_func eq ""){
-      next;
-    }
-    my $bac_line = $line;
 
-    if($processing_macro == 0 and $processing_func == 0){
-      next;
+        my $func_name;
+        if($f =~ /\.c$/){
+            $func_name = is_func_def($line);
+            if($func_name ne "NULL"){
+                $current_func = $func_name;
+                $processing_func = 1;
+    if($indent_overflow == 0){
+        $waiting_func_start = 1;
     }
-    
-    # Parse the line
-    while($line ne ""){
-      if($line =~ /($func_regexp)\s*\((.+)$/){
-        my $call = $1;
-        my $rest = $2;
-	print "$line:\t $call\n";
-        if(is_not_keyword($call)){
-          if($is_macro == 0 or ($is_macro == 1 and $call ne $current_func)){
-            my $caller;
-            if($processing_func == 1){
-              $caller = "func_$files_all{$f}_$current_func";
-            } else {
-              $caller = "macro_$files_all{$f}_$current_func";
+                next;
             }
-            my $callee;
-            if(defined($func_files{$call})){
-              my $ref = $func_files{$call};
-              foreach my $fc (@$ref){
-                if(defined($func_files{$call})){
-                  $callee = "func_$files_all{$fc}_$call";
-                } elsif(defined($macro_files{$call})){
-                  $callee = "macro_$files_all{$fc}_$call";
-                } else{
-                  print STDERR "ERROR: Unknown call $call\n";
-                  next;
+        }
+        $func_name = is_macro($line);
+        my $is_macro = 0;
+        if($func_name ne "NULL"){
+            $is_macro = 1;
+            $current_func = $func_name;
+            $processing_macro = 1;
+            $processing_func = 0;
+        }
+
+        my $bac_line = $line;
+
+        if($current_func eq ""){
+            next;
+        }
+        if($waiting_func_start == 1 and $indent_overflow != 0){
+            $waiting_func_start = 0;
+        }
+
+        if($waiting_func_start == 0 and $indent_count == 0){
+            $processing_func = 0;
+        }
+        if($processing_macro == 0 and $processing_func == 0){
+            next;
+        }
+        
+        # Parse the line
+        while($line ne ""){
+            if($line =~ /($func_regexp)\s*\((.+)$/){
+                my $call = $1;
+                my $rest = $2;
+                if(is_not_keyword($call)){
+                    if($is_macro == 0 or ($is_macro == 1 and $call ne $current_func)){
+                        my $caller;
+                        if($processing_func == 1){
+                            $caller = "func_$files_all{$f}_$current_func";
+                        } else {
+                            $caller = "macro_$files_all{$f}_$current_func";
+                        }
+                        my $callee;
+                        if(defined($func_files{$call})){
+                            my $ref = $func_files{$call};
+                            foreach my $fc (@$ref){
+                                if(defined($func_files{$call})){
+                                    $callee = "func_$files_all{$fc}_$call";
+                                } elsif(defined($macro_files{$call})){
+                                    $callee = "macro_$files_all{$fc}_$call";
+                                } else{
+                                    print STDERR "ERROR: Unknown call $call\n";
+                                    next;
+                                }
+                                if(not defined($call_made{"$caller -> $callee"})){
+                                    $call_made{"$caller -> $callee"} = 1;
+                                    $call_string .= "\t$caller -> $callee\n";
+                                }
+                            }
+                        } else {
+                            $callee = "EXT_$call";
+                            if(not defined($call_made{"$caller -> $callee"})){
+                                $call_string .= "\t$caller -> $callee\n";
+                                $external_funcs{$call} = 1;
+                                $call_made{"$caller -> $callee"} = 1;
+                            }
+                        }
+                    }
+
                 }
-                if(not defined($call_made{"$caller -> $callee"})){
-                  $call_made{"$caller -> $callee"} = 1;
-                  $call_string .= "\t$caller -> $callee\n";
-                }
-              }
+                $line = $rest;
             } else {
-              $callee = "EXT_$call";
-              if(not defined($call_made{"$caller -> $callee"})){
-                $call_string .= "\t$caller -> $callee\n";
-                $external_funcs{$call} = 1;
-                $call_made{"$caller -> $callee"} = 1;
-              }
+                $line = "";
             }
-          }
-
         }
-        $line = $rest;
-      } else {
-        $line = "";
-      }
-    }
 
-    if($processing_func == 1){
-      if($at_least_one == 0){
-        if($bac_line =~ /{/){
-          $at_least_one = 1;
-          $indent_count++;
+        if($processing_macro == 1){
+            if($bac_line =~ /\\\s*$/){
+                $processing_macro = 1;
+            } else {
+                $processing_macro = 0;
+            }
         }
-      }
-      if($bac_line =~ /{/){
-        $indent_count++;
-      }
-      if($bac_line =~ /}/){
-        $indent_count--;
-      }
-      if($indent_count <= 0){
-        $processing_func = 0;
-        $current_func = "";
-      }
+
     }
+    close(IN);
 
-    if($processing_macro == 1){
-      if($bac_line =~ /\\\s*$/){
-        $processing_macro = 1;
-      } else {
-        $processing_macro = 0;
-      }
-    }
-
-  }
-  close(IN);
-
-  return $call_string;
+    return $call_string;
 }
 
 
 sub is_func_def
 {
-	my $l = shift;
+    my $l = shift;
 
-	my @ret_types = (
-		"void",
-		"int",
-		"unsigned int",
-		"unsigned long",
-		"unsigned long int",
-		"unsigned long long",
-		"unsigned long long int",
-		"char",
-		"unsigned char",
-		"long",
-		"long long",
-		"long int",
-		"long long int",
-    "u64",
-    "u32"
-	);
+    my @ret_types = (
+        "void",
+        "int",
+        "unsigned int",
+        "unsigned long",
+        "unsigned long int",
+        "unsigned long long",
+        "unsigned long long int",
+        "char",
+        "unsigned char",
+        "long",
+        "long long",
+        "long int",
+        "long long int",
+        "u64",
+        "u32"
+    );
 
-	my @discreptors = (
-		"",
-		"static",
-		"inline"
-	);
+    my @discreptors = (
+        "",
+        "static",
+        "inline"
+    );
 
-  my @init_exit = (
-    "",
-    "__init",
-    "__exit"
-  );
-	my $func;
-
-	foreach my $key (@ret_types){
-		foreach my $dis (@discreptors){
-      foreach my $post_dis (@init_exit){
-       my $match;
-       if($dis eq ""){
-         $match = "$key";
-       } else {
-         $match = "$dis\\s+$key";
-       }
-       if($post_dis ne ""){
-        $match = $match . "\\s+$post_dis";
-       }
-  			if($l =~ /^\s*$match\s+(\S+)\s*\(/){
-  				return $1;
-  			}
-  		}
+    my @init_exit = (
+        "",
+        "__init",
+        "__exit"
+    );
+    my $func;
+    if($l =~ /;\s*$/){
+        return "NULL";
     }
-	}
-	return "NULL";
+
+    foreach my $key (@ret_types){
+        foreach my $dis (@discreptors){
+            foreach my $post_dis (@init_exit){
+             my $match;
+             if($dis eq ""){
+                 $match = "$key";
+             } else {
+                 $match = "$dis\\s+$key";
+             }
+             if($post_dis ne ""){
+                $match = $match . "\\s+$post_dis";
+             }
+                if($l =~ /^\s*$match\s+(\S+)\s*\(/){
+                    return $1;
+                }
+            }
+        }
+    }
+    return "NULL";
 }
 
 sub is_macro
 {
-	my $l = shift;
-	my $macro;
-	if($l =~/^\s*#define\s+([\d\S]+)\(/){
-		return $1;
-	}
-	return "NULL";
+    my $l = shift;
+    my $macro;
+    if($l =~/^\s*#define\s+([\d\S]+)\(/){
+        return $1;
+    }
+    return "NULL";
 }
 
 sub is_func_use
 {
-  my $l = shift;
-  # XXX Parse line... return list of functions used here. 
+    my $l = shift;
+    # XXX Parse line... return list of functions used here. 
 }
 
 sub exists_in_array
 {
-  my $element = shift;
-  my $arr_ref = shift;
-  foreach my $sub (@$arr_ref){
-    if($element eq $sub){
-      return 1;
+    my $element = shift;
+    my $arr_ref = shift;
+    foreach my $sub (@$arr_ref){
+        if($element eq $sub){
+            return 1;
+        }
     }
-  }
-  return 0;
+    return 0;
 }
 
 sub is_not_keyword
 {
-  my $func = shift;
-  my %keywords = (
-    "for" => 1,
-    "if" => 1,
-    "while" => 1,
-    "sizeof" => 1,
-    "switch" => 1
-  );
-  if(defined($keywords{$func})){
-    return 0;
-  }
-  return 1;
+    my $func = shift;
+    my %keywords = (
+        "for" => 1,
+        "if" => 1,
+        "while" => 1,
+        "sizeof" => 1,
+        "switch" => 1
+    );
+    if(defined($keywords{$func})){
+        return 0;
+    }
+    return 1;
 }
+
+sub count_char
+{
+    my $char = shift;
+    my $line = shift;
+    my $count = 0;
+    my @arr = split(//,$line);
+    foreach my $ch (@arr){
+        if($ch eq $char){
+            $count++;
+        }
+    }
+    return $count;
+}
+
