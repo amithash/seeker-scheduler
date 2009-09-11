6ed9f16762a8801958fa9037a6995836f7cfc370
Amithash Prasad <aeprasad@karmic-laptop.(none)>
2009-09-11 15:07:01 -0600

    fixed copyright

--- Module/pmu/pmu_int.h
+++ Module/pmu/pmu_int.h
@@ -1,6 +1,7 @@
 
 /*****************************************************
- * Copyright 2008 Amithash Prasad                    *
+ * Copyright 2009 Amithash Prasad                    *
+ * Copyright 2006 Tipp Mosely                        *
  *                                                   *
  * This file is part of Seeker                       *
  *                                                   *
