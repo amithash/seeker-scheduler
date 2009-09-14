f80d69efb3f092738e61e8ca7b2ae9808bce1450
Amithash Prasad <aeprasad@linux-e98f.site>
2009-09-13 19:41:09 -0600

    changed license format to span all 80 characters (rather than 50) Added file name and desc section for each file.

--- Module/seeker-scheduler/mutate_common.c
+++ Module/seeker-scheduler/mutate_common.c
@@ -1,26 +1,28 @@
-/*****************************************************
- * Copyright 2009 Amithash Prasad                    *
- *                                                   *
- * This file is part of Seeker                       *
- *                                                   *
- * Seeker is free software: you can redistribute     *
- * it and/or modify it under the terms of the        *
- * GNU General Public License as published by        *
- * the Free Software Foundation, either version      *
- * 3 of the License, or (at your option) any         *
- * later version.                                    *
- *                                                   *
- * This program is distributed in the hope that      *
- * it will be useful, but WITHOUT ANY WARRANTY;      *
- * without even the implied warranty of              *
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR       *
- * PURPOSE. See the GNU General Public License       *
- * for more details.                                 *
- *                                                   *
- * You should have received a copy of the GNU        *
- * General Public License along with this program.   *
- * If not, see <http://www.gnu.org/licenses/>.       *
- *****************************************************/
+/******************************************************************************\
+ * FILE: mutate_common.c
+ * DESCRIPTION: 
+ *
+ \*****************************************************************************/
+
+/******************************************************************************\
+ * Copyright 2009 Amithash Prasad                                              *
+ *                                                                             *
+ * This file is part of Seeker                                                 *
+ *                                                                             *
+ * Seeker is free software: you can redistribute it and/or modify it under the *
+ * terms of the GNU General Public License as published by the Free Software   *
+ * Foundation, either version 3 of the License, or (at your option) any later  *
+ * version.                                                                    *
+ *                                                                             *
+ * This program is distributed in the hope that it will be useful, but WITHOUT *
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or       *
+ * FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License        *
+ * for more details.                                                           *
+ *                                                                             *
+ * You should have received a copy of the GNU General Public License along     *
+ * with this program. If not, see <http://www.gnu.org/licenses/>.              *
+ \*****************************************************************************/
+
 
 #include <linux/kernel.h>
 #include <linux/sched.h>
@@ -30,9 +32,9 @@
 #include <seeker.h>
 #include "mutate.h"
 
-/********************************************************************************
+/*******************************************************************************\
  * 			Function Declarations 					*
- ********************************************************************************/
+\*******************************************************************************/
 
 static void state_change(struct work_struct *w);
 
@@ -42,7 +44,7 @@ void greedy_delta(int dt);
 void mem_dynamic_prog(int dt);
 
 /********************************************************************************
- * 			Global Datastructures 					*
+ * 			Global Data structures 					*
  ********************************************************************************/
 
 /* The mutataor's work struct */
