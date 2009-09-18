57c13c55d39e914eefcfbadc67df5839a90ff17c
Amithash Prasad <prasadae@linux-4g6u.site>
2009-09-18 16:39:39 -0600

    added license to util.

--- utils/launch.c
+++ utils/launch.c
@@ -1,21 +1,28 @@
-/**************************************************************************
- * Copyright 2009 Amithash Prasad                                         *
- *                                                                        *
- * This file is part of multi-launcher                                    *
- *                                                                        *
- * multi-launcher is free software: you can redistribute it and/or modify *
- * it under the terms of the GNU General Public License as published by   *
- * the Free Software Foundation, either version 3 of the License, or      *
- * (at your option) any later version.                                    *
- *                                                                        *
- * This program is distributed in the hope that it will be useful,        *
- * but WITHOUT ANY WARRANTY; without even the implied warranty of         *
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the          *
- * GNU General Public License for more details.                           *
- *                                                                        *
- * You should have received a copy of the GNU General Public License      *
- * along with this program.  If not, see <http://www.gnu.org/licenses/>.  *
- **************************************************************************/
+/******************************************************************************\
+ * FILE: launch.c
+ * DESCRIPTION: The implementation of a untility to run a/set application(s)
+ * on specific cores. Also prints the actual running time of each application.
+ *
+ \*****************************************************************************/
+
+/******************************************************************************\
+ * Copyright 2009 Amithash Prasad                                              *
+ *                                                                             *
+ * This file is part of Seeker                                                 *
+ *                                                                             *
+ * Seeker is free software: you can redistribute it and/or modify it under the *
+ * terms of the GNU General Public License as published by the Free Software   *
+ * Foundation, either version 3 of the License, or (at your option) any later  *
+ * version.                                                                    *
+ *                                                                             *
+ * This program is distributed in the hope that it will be useful, but WITHOUT *
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or       *
+ * FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License        *
+ * for more details.                                                           *
+ *                                                                             *
+ * You should have received a copy of the GNU General Public License along     *
+ * with this program. If not, see <http://www.gnu.org/licenses/>.              *
+ \*****************************************************************************/
 
 #include <stdio.h>
 #include <sched.h>
