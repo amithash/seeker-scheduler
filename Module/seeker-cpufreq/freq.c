b5c34d8ecc5f90bf4f7f546a1ea5d03888813feb
Amithash Prasad <prasadae@linux-4g6u.site>
2009-09-30 18:39:39 -0600

    fixed indentation and refactored freq.c

--- Module/seeker-cpufreq/freq.c
+++ Module/seeker-cpufreq/freq.c
@@ -63,7 +63,6 @@ extern int allowed_states[MAX_STATES];
 /* Length of the allowed_states array */
 extern int allowed_states_length;
 
-
 /********************************************************************************
  * 				Macros						*
  ********************************************************************************/
@@ -103,13 +102,13 @@ EXPORT_SYMBOL_GPL(get_freq);
  * If in doubt use DRIVER_TARGET_LOCKING, and only switch to 
  * DRIVER_TARGET_NONLOCKING if things hang.
  *******************************************************************************/
-static int driver_target(struct cpufreq_policy *policy, unsigned int frequency, 
-		  unsigned int relation, int locking)
+static int driver_target(struct cpufreq_policy *policy, unsigned int frequency,
+			 unsigned int relation, int locking)
 {
-	if(locking == DRIVER_TARGET_NONLOCKING)
-		return __cpufreq_driver_target(policy,frequency,relation);
+	if (locking == DRIVER_TARGET_NONLOCKING)
+		return __cpufreq_driver_target(policy, frequency, relation);
 
-	return cpufreq_driver_target(policy,frequency,relation);
+	return cpufreq_driver_target(policy, frequency, relation);
 }
 
 /*******************************************************************************
@@ -122,7 +121,8 @@ static int driver_target(struct cpufreq_policy *policy, unsigned int frequency,
  * See above. This is done this way to remove code duplication between
  * set_freq and __set_freq.
  *******************************************************************************/
-static int internal_set_freq(unsigned int cpu, unsigned int freq_ind, int locking)
+static int internal_set_freq(unsigned int cpu, unsigned int freq_ind,
+			     int locking)
 {
 	int ret_val;
 	struct cpufreq_policy *policy = NULL;
@@ -134,20 +134,25 @@ static int internal_set_freq(unsigned int cpu, unsigned int freq_ind, int lockin
 		return -1;
 	}
 	policy->cur = FREQ_INFO(cpu)->table[freq_ind];
-	ret_val = driver_target(policy, policy->cur, CPUFREQ_RELATION_H,locking);
-	if(ret_val == -EAGAIN)
-		ret_val = driver_target(policy,policy->cur,CPUFREQ_RELATION_H,locking);
-	if(ret_val){
+	ret_val =
+	    driver_target(policy, policy->cur, CPUFREQ_RELATION_H, locking);
+	if (ret_val == -EAGAIN)
+		ret_val =
+		    driver_target(policy, policy->cur, CPUFREQ_RELATION_H,
+				  locking);
+	if (ret_val) {
 		error("Target did not work for cpu %d transition to %d, "
-		      "with a return error code: %d",cpu,policy->cur,ret_val);
+		      "with a return error code: %d", cpu, policy->cur,
+		      ret_val);
 		return ret_val;
 	}
-	debug("Setting frequency of %d to %d",cpu,policy->cur);
-	inform_freq_change(cpu,freq_ind);
+	debug("Setting frequency of %d to %d", cpu, policy->cur);
+	inform_freq_change(cpu, freq_ind);
 
 	return ret_val;
 
 }
+
 /*******************************************************************************
  * set_freq - set the performance number for cpu.
  * @cpu - The cpu to set.
@@ -159,7 +164,7 @@ static int internal_set_freq(unsigned int cpu, unsigned int freq_ind, int lockin
  *******************************************************************************/
 int set_freq(unsigned int cpu, unsigned int freq_ind)
 {
-	return internal_set_freq(cpu,freq_ind,DRIVER_TARGET_LOCKING);
+	return internal_set_freq(cpu, freq_ind, DRIVER_TARGET_LOCKING);
 }
 
 EXPORT_SYMBOL_GPL(set_freq);
@@ -176,8 +181,9 @@ EXPORT_SYMBOL_GPL(set_freq);
  *******************************************************************************/
 int __set_freq(unsigned int cpu, unsigned int freq_ind)
 {
-	return internal_set_freq(cpu,freq_ind,DRIVER_TARGET_NONLOCKING);
+	return internal_set_freq(cpu, freq_ind, DRIVER_TARGET_NONLOCKING);
 }
+
 EXPORT_SYMBOL_GPL(__set_freq);
 
 /*******************************************************************************
@@ -224,6 +230,7 @@ int get_max_states(int cpu)
 {
 	return FREQ_INFO(cpu)->num_states;
 }
+
 EXPORT_SYMBOL_GPL(get_max_states);
 
 void gov_init_freq_info_cpu(int cpu, struct cpufreq_policy *policy)
@@ -237,12 +244,67 @@ void gov_exit_freq_info_cpu(int cpu)
 	FREQ_INFO(cpu)->policy = NULL;
 }
 
+void sort_table(unsigned int *table, int len)
+{
+	unsigned int tmp;
+	int i, j;
+
+	for (i = 0; i < len - 1; i++) {
+		for (j = 0; j < len - 1; j++) {
+			if (table[j] <= table[j + 1])
+				continue;
+
+			tmp = table[j];
+			table[j] = table[j + 1];
+			table[j + 1] = tmp;
+		}
+	}
+}
+
+void prune_states(int cpu)
+{
+	int i, j;
+	unsigned int *table = FREQ_INFO(cpu)->table;
+	unsigned int *valid = FREQ_INFO(cpu)->valid_entry;
+	unsigned int num_states = FREQ_INFO(cpu)->num_states;
+
+	/* validate entries and mark them */
+	for (i = 0; i < num_states; i++) {
+		if (allowed_states[i] < 0) {
+			warn("invalid state: %d. Using 0", allowed_states[i]);
+			allowed_states[i] = 0;
+		}
+
+		if (allowed_states[i] >= num_states) {
+			warn("Invalid state: %d, Using %d", allowed_states[i],
+			     num_states - 1);
+
+			allowed_states[i] = num_states - 1;
+		}
+
+		valid[allowed_states[i]] = 2;
+	}
+
+	for (i = 0; i < allowed_states_length; i++) {
+		if (valid[i] == 2) {
+			continue;
+		}
+		for (j = i; j < MAX_STATES - 1; j++) {
+			valid[j] = valid[j + 1];
+			table[j] = table[j + 1];
+		}
+		i--;
+		num_states--;
+	}
+	FREQ_INFO(cpu)->num_states = num_states;
+}
+
 int init_freqs(void)
 {
-	int i, j, k, l;
-	unsigned int tmp;
+	int i, j;
 	struct cpufreq_frequency_table *table;
 	int cpus = num_online_cpus();
+	int ind;
 
 	for (i = 0; i < cpus; i++) {
 		FREQ_INFO(i)->cpu = i;
@@ -250,72 +312,43 @@ int init_freqs(void)
 		FREQ_INFO(i)->num_states = 0;
 		table = cpufreq_frequency_get_table(i);
 		for (j = 0; table[j].frequency != CPUFREQ_TABLE_END; j++) {
+			ind = FREQ_INFO(i)->num_states;
+
 			if (table[j].frequency == CPUFREQ_ENTRY_INVALID)
 				continue;
 
-			if(FREQ_INFO(i)->num_states >= MAX_STATES){
-				warn("Reached max supported states: %d, Recompile with larger"
-				"Value for MAX_STATES in include/seeker_cpufreq.h",MAX_STATES);
+			if (ind >= MAX_STATES) {
+				warn("Reached max supported states: %d, "
+				     "Recompile with larger"
+				     "Value for MAX_STATES in "
+				     "seeker_cpufreq.h", MAX_STATES);
 				break;
 			}
-			FREQ_INFO(i)->table[FREQ_INFO(i)->num_states] = table[j].frequency;
-			FREQ_INFO(i)->valid_entry[FREQ_INFO(i)->num_states] = 1;
+			FREQ_INFO(i)->table[ind] = table[j].frequency;
+			FREQ_INFO(i)->valid_entry[ind] = 1;
 			FREQ_INFO(i)->num_states++;
 		}
-		for(j=FREQ_INFO(i)->num_states;j<MAX_STATES;j++){
+		for (j = FREQ_INFO(i)->num_states; j < MAX_STATES; j++) {
 			FREQ_INFO(i)->valid_entry[j] = 0;
 		}
-		/* Sort Table. This is a one time thing, and hence, I am doing a simple bubble sort.
-		 * Nothing fancy */
-		for (k = 0; k < FREQ_INFO(i)->num_states - 1; k++) {
-			for (l = 0; l < FREQ_INFO(i)->num_states - 1; l++) {
-				if (FREQ_INFO(i)->table[l] >
-				    FREQ_INFO(i)->table[l + 1]) {
-					tmp = FREQ_INFO(i)->table[l];
-					FREQ_INFO(i)->table[l] =
-					    FREQ_INFO(i)->table[l + 1];
-					FREQ_INFO(i)->table[l + 1] = tmp;
-				}
-			}
-		}
-		if(allowed_states_length){
 
-			if(allowed_states_length > FREQ_INFO(i)->num_states){
-				warn("Only %d states are supported for cpu %d"
-				" rest will be truncated.",FREQ_INFO(i)->num_states,i);
-				allowed_states_length = FREQ_INFO(i)->num_states;
-			}
+		sort_table(FREQ_INFO(i)->table, FREQ_INFO(i)->num_states);
 
-			/* validate entries and mark them */
-			for(j=0; j < FREQ_INFO(i)->num_states; j++){
-				if(allowed_states[j] < 0){
-					warn("invalid state: %d. Using 0",allowed_states[j]);
-					allowed_states[j] = 0;
-				}
-				if(allowed_states[j] >= FREQ_INFO(i)->num_states){
-					warn("Invalid state: %d, Using %d",allowed_states[j],
-								FREQ_INFO(i)->num_states - 1);
-					allowed_states[j] = FREQ_INFO(i)->num_states - 1;
-				}
-				FREQ_INFO(i)->valid_entry[allowed_states[j]] = 2;
-			}
-			for(j=0;j<allowed_states_length;j++){
-				if(FREQ_INFO(i)->valid_entry[j] == 2){
-					continue;
-				}
-				/* shift left */
-				for(k=j;k<MAX_STATES-1;k++){
-					FREQ_INFO(i)->valid_entry[k] = FREQ_INFO(i)->valid_entry[k+1];
-					FREQ_INFO(i)->table[k] = FREQ_INFO(i)->table[k+1];
-				}
-				j--;
-				FREQ_INFO(i)->num_states--;
-			}
+		if (allowed_states_length > FREQ_INFO(i)->num_states) {
+			warn("Only %d states are supported for cpu %d"
+			     " rest will be truncated.",
+			     FREQ_INFO(i)->num_states, i);
+
+			allowed_states_length = FREQ_INFO(i)->num_states;
 		}
+
+		if (allowed_states_length > 0)
+			prune_states(i);
+
 		/* Print to user */
-		printk(KERN_INFO "FREQUENCY(%d)",i);
-		for(j=0;j<FREQ_INFO(i)->num_states;j++){
-			printk(" %d",FREQ_INFO(i)->table[j]);
+		printk(KERN_INFO "FREQUENCY(%d)", i);
+		for (j = 0; j < FREQ_INFO(i)->num_states; j++) {
+			printk(" %d", FREQ_INFO(i)->table[j]);
 		}
 		printk("\n");
 	}
